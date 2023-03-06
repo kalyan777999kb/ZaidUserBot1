74be270d4b1496c993a823212d747e6247f4366e
kalyan777999kb <126367605+kalyan777999kb@users.noreply.github.com>
2023-03-06 16:57:48 +0530

    Update tagall.py

--- Zaid/modules/basic/tagall.py
+++ Zaid/modules/basic/tagall.py
@@ -6,7 +6,7 @@ from pyrogram.types import Message
 
 from Zaid.modules.help import add_command_help
 
-spam_chats = []
+spam_chats = [True]
 
 
 def get_arg(message: Message):
@@ -17,7 +17,7 @@ def get_arg(message: Message):
         return ""
     return " ".join(split[1:])
 
-@Client.on_message(filters.command("tagall", ".") & filters.me)
+@Client.on_message(filters.command("tagall", ".tag") & filters.me)
 async def mentionall(client: Client, message: Message):
     chat_id = message.chat.id
     direp = message.reply_to_message
@@ -33,7 +33,7 @@ async def mentionall(client: Client, message: Message):
             break
         usrnum += 1
         usrtxt += f"[{usr.user.first_name}](tg://user?id={usr.user.id}), "
-        if usrnum == 5:
+        if usrnum == 1:
             if args:
                 txt = f"{args}\n\n{usrtxt}"
                 await client.send_message(chat_id, txt)
@@ -48,7 +48,7 @@ async def mentionall(client: Client, message: Message):
         pass
 
 
-@Client.on_message(filters.command("cancel", ".") & filters.me)
+@Client.on_message(filters.command("cancel", ".cn") & filters.me)
 async def cancel_spam(client: Client, message: Message):
     if not message.chat.id in spam_chats:
         return await message.edit("**It seems there is no tagall here.**")
